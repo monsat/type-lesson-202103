ad6025e975fc989c989c3b2846d6deb3eda23bfb
TANAKA Kohji <tanaka.kohji@gmail.com>
2021-03-28 22:08:32 +0900

    Q 3 - 1 answer, excellent!

--- q03-01/main.ts
+++ q03-01/main.ts
@@ -1,4 +1,4 @@
-function mapFromArray(arr, key) {
+function mapFromArray<T, K extends keyof T>(arr: T[], key: K): Map<T[K], T> {
   const result = new Map();
   for (const obj of arr) {
     result.set(obj[key], obj);
